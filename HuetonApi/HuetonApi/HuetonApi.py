b8f8fd3013ccd1646723fd40e583e61194e2f032
bodiam <erik.pragt@jworks.nl>
2013-08-28 23:40:16 +0200

    Implemented part of Configuration API to register users

--- HuetonApi/HuetonApi/HuetonApi.py
+++ HuetonApi/HuetonApi/HuetonApi.py
@@ -2,24 +2,28 @@ import requests
 import json
 import sys
 
+
 class HuetonApi:
-    
     def init(self, developer_name):
         print("Hello %s" % developer_name)
         self.registered = False
+        self.base_location = "http://192.168.2.196/api/"
         self.location = "http://192.168.2.196/api/" + developer_name
+        self.developer_name = developer_name
 
     def connect(self):
-        
-        print("Connect")
 
-        if self.registered:
-            return True
-        else:
-            return False
+        result = self.hue_get("")
+        parsed = json.loads(result)
+        return 'success' in parsed[0]
 
     def register(self):
-        print("register")
+        print("Remember to press the button on the hub.")
+
+        payload = json.dumps({"devicetype": "test user", "username": self.developer_name})
+        response = requests.post(self.base_location, data=payload)
+
+        print(response)
 
         self.registered = True
 
@@ -27,31 +31,30 @@ class HuetonApi:
         result = self.hue_get("/lights/" + lamp_number)
         parsed = json.loads(result)
         state = parsed['state']['on']
-    
-        return state    
-    
+
+        return state
+
     def hue_set_lamp_color(self, lamp_number, saturation, brightness, hue):
         # {"on":true, "sat":255, "bri":255,"hue":10000}
-        payload = json.dumps({"on":True, "sat": int(saturation), "bri": int(brightness), "hue": int(hue)})
+        payload = json.dumps({"on": True, "sat": int(saturation), "bri": int(brightness), "hue": int(hue)})
         self.hue_set_lamp_state(lamp_number, payload)
-    
+
     def hue_turn_lamp_on(self, lamp_number):
-        payload = json.dumps({"on":True})
+        payload = json.dumps({"on": True})
         self.hue_set_lamp_state(lamp_number, payload)
-    
+
     def hue_turn_lamp_off(self, lamp_number):
-        payload = json.dumps({"on":False})
+        payload = json.dumps({"on": False})
         self.hue_set_lamp_state(lamp_number, payload)
-    
+
     def hue_set_lamp_state(self, lamp_number, payload):
-        self.hue_put("/lights/"+ lamp_number + "/state", payload)
-    
+        self.hue_put("/lights/" + lamp_number + "/state", payload)
+
     def hue_get(self, function):
-        return requests.get(self.location + function).content
-    
+        return requests.get(self.location + function).text
+
     def hue_put(self, function, payload):
         return requests.put(self.location + function, data=payload)
-    
-    
-    
-    
+
+    def hue_post(self, function, payload):
+        return requests.post(self.location + function, data=payload)
--- Playground/apitests/apitest.py
+++ Playground/apitests/apitest.py
@@ -10,156 +10,161 @@ location = "http://192.168.2.196/api/newdeveloper"
 # Hue API
 #
 def hue_get_lamp_state(lamp_number):
-	result = hue_get("/lights/" + lamp_number)
-	parsed = json.loads(result)
-	state = parsed['state']['on']
+    result = hue_get("/lights/" + lamp_number)
+    parsed = json.loads(result)
+    state = parsed['state']['on']
 
-	return state	
+    return state
+
 
 def hue_set_lamp_color(lamp_number, saturation, brightness, hue):
-	# {"on":true, "sat":255, "bri":255,"hue":10000}
-	payload = json.dumps({"on":True, "sat": int(saturation), "bri": int(brightness), "hue": int(hue)})
-	hue_set_lamp_state(lamp_number, payload)
+    # {"on":true, "sat":255, "bri":255,"hue":10000}
+    payload = json.dumps({"on": True, "sat": int(saturation), "bri": int(brightness), "hue": int(hue)})
+    hue_set_lamp_state(lamp_number, payload)
+
 
 def hue_turn_lamp_on(lamp_number):
-	payload = json.dumps({"on":True})
-	hue_set_lamp_state(lamp_number, payload)
+    payload = json.dumps({"on": True})
+    hue_set_lamp_state(lamp_number, payload)
+
 
 def hue_turn_lamp_off(lamp_number):
-	payload = json.dumps({"on":False})
-	hue_set_lamp_state(lamp_number, payload)
+    payload = json.dumps({"on": False})
+    hue_set_lamp_state(lamp_number, payload)
+
 
 def hue_set_lamp_state(lamp_number, payload):
-	hue_put("/lights/"+ lamp_number + "/state", payload)
+    hue_put("/lights/" + lamp_number + "/state", payload)
+
 
 def hue_get(function):
-	return requests.get(location + function).content
+    return requests.get(location + function).text
+
 
 def hue_put(function, payload):
-	return requests.put(location + function, data=payload)
+    return requests.put(location + function, data=payload)
 
 #
 # Main programs
 #
 def clear_screen():
+    print
 
-	print
-	#print chr(27) + "[2J"
+#print chr(27) + "[2J"
 
 def show_menu():
+    menu = defaultdict(list, {
+    '1': [register_new_user, 'Register new user'],
+    '2': [show_all_lamps, 'Show all lamps'],
+    '3': [show_turn_lamps_on_off, 'Turn lamps on/off'],
+    '4': [show_colorize_lamps, 'Colorize lamps'],
+    '5': [use_leapmotion, 'Use leapmotion'],
+    'E': [sys.exit, 'Exit']
+    })
 
-	menu = defaultdict(list, { 
-		'1':[register_new_user,'Register new user'],
-		'2':[show_all_lamps,'Show all lamps'],
-	        '3':[show_turn_lamps_on_off,'Turn lamps on/off'],
-	        '4':[show_colorize_lamps,'Colorize lamps'],
-	        '5':[use_leapmotion,'Use leapmotion'],
-	        'E':[sys.exit, 'Exit']
-	})
+    print("")
+    for key in sorted(menu.keys()):
+        print("%s : %s" % (key, menu[key][1]))
 
-	print ""
-	for key in sorted(menu.keys()):
-		print "%s : %s" % (key, menu[key][1])
+    choice = input("Please choose a number: ")
 
-	choice = raw_input("Please choose a number: ")
+    if not menu[choice]:
+        print("Invalid input: %s" % choice)
+        show_menu()
 
-	if not menu[choice]:
-		print "Invalid input: %s" % choice
-		show_menu()
+    clear_screen()
 
-	clear_screen()
-
-	func = menu[choice][0]
-	func()
-
+    func = menu[choice][0]
+    func()
 
 
 def register_new_user():
+    print("register new user")
+    show_menu()
 
-	print "register new user"
-	show_menu()
 
 def show_all_lamps():
+    print_lamps()
 
-	print_lamps()
+    show_menu()
 
-	show_menu()
 
 def print_lamps():
+    title("Show all lamps")
 
-        title("Show all lamps")
+    # {"1":{"name": "Hue Lamp 1"},"2":{"name": "Hue Lamp 2"},"3":{"name": "Hue Lamp 3"}}
+    result = hue_get("/lights")
+    parsed = json.loads(result)
 
-        # {"1":{"name": "Hue Lamp 1"},"2":{"name": "Hue Lamp 2"},"3":{"name": "Hue Lamp 3"}}
-        result = hue_get("/lights")
-        parsed = json.loads(result)
+    for key in sorted(parsed.keys()):
+        state = hue_get_lamp_state(key)
+        print("lamp:%s, name=%s, state=%s" % (key, parsed[key]['name'], state))
 
-        for key in sorted(parsed.keys()):
-		state = hue_get_lamp_state(key)
-                print "lamp:%s, name=%s, state=%s" % (key, parsed[key]['name'], state)
 
-def title(text): 
-	print text + ":\n"
-
+def title(text):
+    print(text + ":\n")
 
 
 def show_turn_lamps_on_off():
+    title("Turn lamps [on/off]:")
 
-	title("Turn lamps [on/off]:")
+    print_lamps()
 
-	print_lamps()
-		
-	# Choose the lamp
-	lamp_number = get_input_lamp_number()
-	state = hue_get_lamp_state(lamp_number)
+    # Choose the lamp
+    lamp_number = get_input_lamp_number()
+    state = hue_get_lamp_state(lamp_number)
 
-	# Choose the action
-	input = raw_input("Turn lamp %s [on] or [off]: " % lamp_number)
+    # Choose the action
+    input = input("Turn lamp %s [on] or [off]: " % lamp_number)
 
-	if input == "on":
-		hue_turn_lamp_on(lamp_number)
-	else:
-		hue_turn_lamp_off(lamp_number)
+    if input == "on":
+        hue_turn_lamp_on(lamp_number)
+    else:
+        hue_turn_lamp_off(lamp_number)
 
-	show_turn_lamps_on_off()
+    show_turn_lamps_on_off()
+
 
 def get_input_lamp_number():
-	lamp_number = raw_input("Please choose a lamp number (E=Exit menu): ")
+    lamp_number = input("Please choose a lamp number (E=Exit menu): ")
 
-	if not lamp_number:
-		turn_lamps_on_off()
+    if not lamp_number:
+        show_turn_lamps_on_off()
 
-	if lamp_number == "E":
-		show_all_lamps()
+    if lamp_number == "E":
+        show_all_lamps()
 
-	return lamp_number
+    return lamp_number
 
 
 def show_colorize_lamps():
+    title("Colorize lamps")
 
-	title("Colorize lamps")
+    print_lamps()
 
-	print_lamps()
+    # Choose the lamp
+    lamp_number = get_input_lamp_number()
 
-	# Choose the lamp
-	lamp_number = get_input_lamp_number()
+    # TODO: read current lamp settings
 
-	# TODO: read current lamp settings
+    # Choose the action
+    saturation = read_input("Saturation (intesity) [0-255] :") or '255'
+    brightness = read_input("Brightness [0-255] :") or '255'
+    hue = read_input("Hue [0-65535] :") or '10000'
 
-	# Choose the action
-	saturation = read_input("Saturation (intesity) [0-255] :") or '255'
-	brightness = read_input("Brightness [0-255] :") or '255'
-	hue = read_input("Hue [0-65535] :") or '10000'
+    hue_set_lamp_color(lamp_number, saturation, brightness, hue)
 
-	hue_set_lamp_color(lamp_number, saturation, brightness, hue)
+    show_menu()
 
-	show_menu()
 
 def use_leapmotion():
-	print "Use leapmotion"
-	show_menu()
+    print("Use leapmotion")
+    show_menu()
+
 
 def read_input(text):
-	return raw_input(text)
+    return input(text)
+
 
 show_menu()
 
