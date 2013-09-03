7324c2b7a7ae6ba041b5d6e37ec721bb79e693b5
nekun <enric.ballo@gmail.com>
2013-09-03 20:27:52 +0200

    GroupsApi + Group - Initial Commit

---
+++ HuetonApi/HuetonApi/GroupsApi.py
@@ -0,0 +1,82 @@
+# Author: Enric Ballo
+
+# GroupsApi Class
+# We can have more than one group, and each group have one name and a list of lights
+# Example:
+# Groups['kitchenGroup','livingRoomGroup'...]
+class GroupsApi():
+    
+    def __init__(self):
+        self.groups = []
+            
+    def addGroup(self, groupToAdd):
+        self.groups.append(groupToAdd)
+    
+    def removeGroup(self, group):
+        self.groups.remove(group)
+    
+    def addLightToGroup(self, group, lightToAdd):
+        isGroup=False
+        for i in self.groups:
+            if(group == i):
+                isGroup=True
+                i.addLight(lightToAdd)
+                
+        if(isGroup == False):
+            print("The Group dosen't exists")
+    
+    def removeLightFromGroup(self, group, lightToRemove):
+        isGroup=False
+        for i in self.groups:
+            if(group == i):
+                isGroup=True
+                i.removeLight(lightToRemove)
+                
+        if(isGroup == False):
+            print("The Group dosen't exists")
+            
+    def getTotalLightsForGroup(self, group):
+        isGroup = False
+        total = 0
+        for i in self.groups:
+            if(group == i):
+                isGroup=True
+                total = i.get_all_lights()
+                
+        if(isGroup == False):
+            print("The Group dosen't exists")
+            
+        return total
+    
+    def get_all_groups(self):
+        for i in self.groups:
+            print(i.printDetails())
+        return len(self.groups)
+
+# Group Class
+# Example:
+# kitchen (lamp1, lamp2, lamp3)        
+class Group():
+    
+    def __init__(self, name):
+        self.name = name
+        self.listOfLights=[]
+    
+    def changeNameGroup(self, newName):
+        self.name = newName
+    
+    def addLight(self, light):
+        self.listOfLights.append(light)
+    
+    def removeLight(self, light):
+        self.listOfLights.remove(light)
+    
+    def get_all_lights(self):
+        return len(self.listOfLights)
+        
+    def printDetails(self):
+        print("Name : "+self.name)
+        count = 0
+        for i in self.listOfLights:
+            print(str(count) + " - Light "+i)
+        
---
+++ HuetonApi/test/test_GroupsApi.py
@@ -0,0 +1,74 @@
+import unittest
+
+from HuetonApi.GroupsApi import GroupsApi,Group
+
+# Test for GroupsApi Class
+class TestGroupsApi(unittest.TestCase):
+    
+    def test_addGroup(self):
+        gApi = GroupsApi()
+        group = Group("Test Group")
+        gApi.addGroup(group)
+        
+        self.assertEqual(1, len(gApi.groups))
+        
+    def test_removeGroup(self):
+        gApi = GroupsApi()
+        group1 = Group("Test Group 1")
+        group2 = Group("Test Group 2")
+        
+        gApi.addGroup(group1)
+        gApi.addGroup(group2)
+        
+        self.assertEqual(2, len(gApi.groups))
+        
+        gApi.removeGroup(group2)
+        
+        self.assertEqual(1, len(gApi.groups))
+        
+    def test_addLightToGroup(self):
+        gApi = GroupsApi()
+        group1 = Group("Test Group 1")
+        gApi.addGroup(group1)
+        
+        light = "will be a light"
+        gApi.addLightToGroup(group1, light)
+        
+    def test_removeLightFromGroup(self):
+        gApi = GroupsApi()
+        group1 = Group("Test Group 1")
+        gApi.addGroup(group1)
+        
+        light1 = "will be a light 1"
+        light2 = "will be a light 2"
+        gApi.addLightToGroup(group1, light1)
+        gApi.addLightToGroup(group1, light2)
+        
+        self.assertEqual(2, gApi.getTotalLightsForGroup(group1))
+        
+        gApi.get_all_groups()
+        
+        gApi.removeLightFromGroup(group1, light2)
+        
+        self.assertEqual(1, gApi.getTotalLightsForGroup(group1))
+        
+        gApi.get_all_groups()
+    
+    def test_get_all_Groups(self):
+        gApi = GroupsApi()
+        
+        group1 = Group("Group1")
+        group2 = Group("Group2")
+        group3 = Group("Group3")
+        
+        gApi.addGroup(group1)
+        gApi.addGroup(group2)
+        gApi.addGroup(group3)
+        
+        total = gApi.get_all_groups()
+        
+        self.assertEqual(3, total)
+
+
+if __name__ == '__main__':
+    unittest.main()
