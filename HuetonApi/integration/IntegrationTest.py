610c377009a266ee327e6e98443e78da8dff5756
Erik Pragt <erik.pragt@jworks.nl>
2013-12-28 00:30:29 +0100

    Added Python logging API

--- HuetonApi/integration/IntegrationTest.py
+++ HuetonApi/integration/IntegrationTest.py
@@ -1,5 +1,8 @@
 import unittest
 import time
+import logging
+
+from logging import Formatter
 
 from HuetonApi.LightsApi import LightsApi
 from HuetonApi.ConfigurationApi import ConfigurationApi
@@ -12,26 +15,30 @@ class IntegrationTest(unittest.TestCase):
 
         self.light_api = LightsApi('huetonuser', 'http://192.168.2.196/api/')
 
-        # self.logger = logging.getLogger('hueton')
-        # self.logger.setLevel(logging.DEBUG)
+        logger = logging.getLogger('hueton')
+        logger.handlers = []  # clear handlers
+        logger.setLevel(logging.DEBUG)
 
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        logger.addHandler(console_handler)
+
+        self.logger = logger
 
     def test_scenario_diagnostics(self):
         lights = self.light_api.get_all_lights()
 
-        [print(repr(light)) for light in lights]
-
+        [self.logger.debug(repr(light)) for light in lights]
 
     def test_scenario_turn_lights_on_and_off(self):
         lights = self.light_api.get_all_lights()
 
         for _ in range(3):
-            # self.logger.debug("Off")
-            print("Turn lights off")
-
+            self.logger.debug("Turn lights off")
             [self.light_api.hue_turn_lamp_off(light.id) for light in lights]
             time.sleep(1)
 
-            print("Turn lights on")
+            self.logger.debug("Turn lights on")
             [self.light_api.hue_turn_lamp_on(light.id) for light in lights]
             time.sleep(1)
